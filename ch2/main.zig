473d4139b4bcabe5ec75ba8f36b116be290e5a24
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-06-08 13:55:46 +0300

    cont

--- ch2/main.zig
+++ ch2/main.zig
@@ -113,3 +113,140 @@ test "make dir" {
 
     try expect(file_count == 3);
 }
+
+test "io writer usage" {
+    var list = ArrayList(u8).init(test_allocator);
+    defer list.deinit();
+    const bytes_written = try list.writer().write(
+        "Hello World!",
+    );
+    try expect(bytes_written == 12);
+    try expect(eql(u8, list.items, "Hello World!"));
+}
+
+test "io reader usage" {
+    const message = "Hello File!";
+
+    const file = try std.fs.cwd().createFile("junk_file2.txt", .{ .read = true });
+    defer file.close();
+
+    try file.writeAll(message);
+    try file.seekTo(0);
+
+    const contents = try file.reader().readAllAlloc(test_allocator, message.len);
+    defer test_allocator.free(contents);
+
+    try expect(eql(u8, contents, message));
+}
+
+fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
+    var line = (try reader.readUntilDelimiterOrEof(buffer, '\n')) orelse return null;
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line, "\r");
+    } else {
+        return line;
+    }
+}
+
+// test "read until next line" {
+//     const stdout = std.io.getStdOut();
+//     const stdin = std.io.getStdIn();
+
+//     try stdout.writeAll(
+//         \\ Enter your name:
+//     );
+
+//     var buffer: [100]u8 = undefined;
+//     const input = (try nextLine(stdin.reader(), &buffer)).?;
+//     try stdout.writer().print("Your name is: \"{s}\"\n", .{input});
+// }
+
+const MyByteList = struct {
+    data: [100]u8 = undefined,
+    items: []u8 = &[_]u8{},
+
+    const Writer = std.io.Writer(
+        *MyByteList,
+        error{EndOfBuffer},
+        appendWrite,
+    );
+
+    fn appendWrite(self: *MyByteList, data: []const u8) error{EndOfBuffer}!usize {
+        if (self.items.len + data.len > self.data.len) {
+            return error.EndOfBuffer;
+        }
+
+        std.mem.copy(u8, self.data[self.items.len..], data);
+        self.items = self.data[0 .. self.items.len + data.len];
+        return data.len;
+    }
+
+    fn writer(self: *MyByteList) Writer {
+        return .{ .context = self };
+    }
+};
+
+test "custom writer" {
+    var bytes = MyByteList{};
+    _ = try bytes.writer().write("Hello");
+    _ = try bytes.writer().write(" Writer!");
+    try expect(eql(u8, bytes.items, "Hello Writer!"));
+}
+
+test "fmt" {
+    const string = try std.fmt.allocPrint(test_allocator, "{d} + {d} = {d}", .{ 9, 10, 19 });
+    defer test_allocator.free(string);
+    try expect(eql(u8, string, "9 + 10 = 19"));
+}
+
+test "print" {
+    var list = std.ArrayList(u8).init(test_allocator);
+    defer list.deinit();
+    try list.writer().print("{} + {} = {}", .{ 9, 10, 19 });
+    try expect(eql(u8, list.items, "9 + 10 = 19"));
+}
+
+// test "hello world" {
+//     const out_file = std.io.getStdOut();
+//     try out_file.writer().print("Hello, {s}!\n", .{"World"});
+// }
+
+test "array printing" {
+    const string = try std.fmt.allocPrint(test_allocator, "{any} + {any} = {any}", .{
+        @as([]const u8, &[_]u8{ 1, 4 }),
+        @as([]const u8, &[_]u8{ 2, 5 }),
+        @as([]const u8, &[_]u8{ 3, 9 }),
+    });
+    defer test_allocator.free(string);
+
+    try expect(eql(u8, string, "{ 1, 4 } + { 2, 5 } = { 3, 9 }"));
+}
+
+const Person = struct {
+    name: []const u8,
+    birth_year: i32,
+    death_year: ?i32,
+
+    pub fn format(self: Person, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.print("{s} ({}-", .{ self.name, self.birth_year });
+        if (self.death_year) |year| try writer.print("{}", .{year});
+        try writer.writeAll(")");
+    }
+};
+
+test "custom fmt" {
+    const john = Person{ .name = "John Carmack", .birth_year = 1970, .death_year = null };
+    const john_string = try std.fmt.allocPrint(test_allocator, "{s}", .{john});
+    defer test_allocator.free(john_string);
+
+    try expect(eql(u8, john_string, "John Carmack (1970-)"));
+
+    const claude = Person{ .name = "Claude Shannon", .birth_year = 1916, .death_year = 2001 };
+    const claude_string = try std.fmt.allocPrint(test_allocator, "{s}", .{claude});
+    defer test_allocator.free(claude_string);
+
+    try expect(eql(u8, claude_string, "Claude Shannon (1916-2001)"));
+}
