e8f1a71f55d8e4c7add1cc43a53059d89662d20f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-06-08 15:28:40 +0300

    cont

--- ch2/main.zig
+++ ch2/main.zig
@@ -442,3 +442,40 @@ test "iterator looping" {
     }
     try expect(file_count > 0);
 }
+
+// test "arg iteration" {
+//     var arg_characters: usize = 0;
+//     var iter = std.process.args();
+//     while (try iter.next()) |arg| {
+//         arg_characters += arg.len;
+//         // test_allocator.free(arg);
+//     }
+//     try expect(arg_characters > 0);
+// }
+
+const ContainsIterator = struct {
+    strings: []const []const u8,
+    needle: []const u8,
+    index: usize = 0,
+    fn next(self: *ContainsIterator) ?[]const u8 {
+        const index = self.index;
+        for (self.strings[index..]) |string| {
+            self.index += 1;
+            if (std.mem.indexOf(u8, string, self.needle)) |_| {
+                return string;
+            }
+        }
+        return null;
+    }
+};
+
+test "custom iterator" {
+    var iter = ContainsIterator{
+        .strings = &[_][]const u8{ "one", "two", "three" },
+        .needle = "e",
+    };
+
+    try expect(eql(u8, iter.next().?, "one"));
+    try expect(eql(u8, iter.next().?, "three"));
+    try expect(iter.next() == null);
+}
